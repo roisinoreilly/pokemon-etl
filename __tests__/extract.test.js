6022fab80240329ca1c29398dbd16a1871f39fc8
roisinoreilly <105567402+roisinoreilly@users.noreply.github.com>
2023-06-20 09:24:39 +0100

    testing

--- __tests__/extract.test.js
+++ __tests__/extract.test.js
@@ -1,6 +1,31 @@
-const pokemon = require("../mappedPokemon.json");
-const moves = require("../moves.json");
-const types = require("../types.json");
+const pokemon = require("../data/mappedPokemon.json");
+const moves = require("../data/moves.json");
+const types = require("../data/types.json");
+const allPokemon = require("../data/pokemon.json");
+
+describe('original Pokemon dataset', () => {
+  test('should have a length of 151', () => {
+    expect(allPokemon.length).toBe(151);
+  });
+});
+
+describe('mappedPokemon dataset', () => {
+  test('should have a length of 151', () => {
+    expect(pokemon.length).toBe(151);
+  });
+  test('should have the keys of name, id, height, weight, moves and types', () => {
+    pokemon.forEach((pokemon) => {
+      expect(pokemon).toHaveProperty("name");
+      expect(pokemon).toHaveProperty("id");
+      expect(pokemon).toHaveProperty("height");
+      expect(pokemon).toHaveProperty("weight");
+      expect(pokemon).toHaveProperty("moves");
+      expect(pokemon).toHaveProperty("types");
+    })
+  });
+});
+
+describe("")
 
 describe("Moves and Type ids", () => {
   test("All pokemon moves names and ids should line up with moves object", () => {
